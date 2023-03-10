a21ce733c21a66051dada1b6304f2b73e12c6cbe
yuen1222 <11160242@me.mcu.edu.tw>
2023-03-10 11:47:47 +0800

    week04

---
+++ week04/week04-1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a=10;
+    printf("a的值是%d\n",a);
+    printf("a的家在%d\n",&a);
+    int b=20;
+    printf("b的值是%d\n",a);
+    printf("b的家在%d\n",&a);
+    int c=30;
+    printf("c的值是%d\n",a);
+    printf("c的家在%d\n",&a);
+}
